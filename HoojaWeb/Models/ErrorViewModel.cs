8bbe726b2060406ee714c35ce23eb85d406898d7
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-05-22 11:44:55 +0200

    Add project files.

---
+++ HoojaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoojaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
