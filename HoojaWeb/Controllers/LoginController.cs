0f6a1fcd69e9f1e3e5b3cf72eb3e754236774556
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-05-23 22:42:32 +0200

    Beginning of layout

---
+++ HoojaWeb/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoojaWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminView()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerView()
+        {
+            return View();
+        }
+    }
+}
