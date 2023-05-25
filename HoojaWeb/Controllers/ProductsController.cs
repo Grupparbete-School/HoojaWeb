ce3a27180de12e196b148bb43e634bdcc93b46c6
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-05-26 00:15:59 +0200

    Products, sorting, cart, orderDetails customer, homepage updated

--- HoojaWeb/Controllers/ProductsController.cs
+++ HoojaWeb/Controllers/ProductsController.cs
@@ -13,5 +13,10 @@ namespace HoojaWeb.Controllers
         {
             return View();
         }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
     }
 }
