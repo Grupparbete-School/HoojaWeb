5847199b0bf7d31dc8911baf1bcb1d3001dc6d32
ullzten <ullzten@gmail.com>
2023-05-30 23:00:17 +0200

    add googlemap

---
+++ HoojaWeb/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoojaWeb.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
