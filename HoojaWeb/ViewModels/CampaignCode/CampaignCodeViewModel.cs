9cab3e62279ac9d30313873461f884d1302f8c4e
malin eriksson <erixon.malin@hotmail.com>
2023-06-03 15:43:12 +0200

    fixes campainmethod

--- HoojaWeb/ViewModels/CampaignCode/CampaignCodeViewModel.cs
+++ HoojaWeb/ViewModels/CampaignCode/CampaignCodeViewModel.cs
@@ -5,5 +5,11 @@ namespace HoojaWeb.ViewModels.CampaignCode
         public int? CampaignCodeId { get; set; }
 
         public string? CampaignName { get; set; }
+
+        public DateTime? CampaignStart { get; set; }
+
+        public DateTime? CampaignEnd { get; set; }
+
+        public decimal? DiscountPercentage { get; set; }
     }
 }
